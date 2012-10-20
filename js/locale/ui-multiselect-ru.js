5ea962d30cbe42cc17d02cfa160c67a484fe36d8
Serg N. Kalachev <serg@kalachev.ru>
2012-10-20 19:19:34 +0400

    Added Russian translation

---
+++ js/locale/ui-multiselect-ru.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale en, en-US
+ */
+
+$.extend($.ui.multiselect.locale, {
+	addAll:'Добавить все',
+	removeAll:'Удалить все',
+	itemsCount:'элементов выбрано'
+});
