28870e73f50c6c5ba8a0ad8f2b5a3aa548aff00c
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2013-03-13 17:46:16 +0100

    Add hungarian translations

---
+++ js/locale/ui-multiselect-hu.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale hu, hu-HU
+ */
+
+$.extend($.ui.multiselect.locale, {
+	addAll:     'Összes hozzáadása',
+	removeAll:  'Összes eltávolítása',
+	itemsCount: 'kiválasztott elem'
+});
